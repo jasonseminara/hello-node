e36fcf8bc264b3bd2346deef01d5dc9473a13c7c
Jason Seminara <jseminara@gmail.com>
2017-04-02 22:41:48 -0400

    changed port to 8081

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# hello-node
+# AWS demo
+
+## Getting set up
+1. Use `git clone` to bring this website code down from Github.
+1. Use `npm i` to install all dependencies.
+1. Use `brew install awsebcli` to install the AWS Elastic Beanstalk CLI toolkit.
+
+## Init a new AWS EB application 
+1. `eb init` 
+1. choose the default region
+1. (You'll get an error about access. Let's fix that.)
+  1. Generate access keys: your name > 'My Security Credentials' > Access Keys > Create New Access Key
+  1. paste the id and key into the wizard
+1. Auto-detect Node.js
+1. CodeCommit? yes
+
+
+## Create a new AWS EB application
+1. `eb create` (follow the prompts and wait for the service to restart)
--- server.js
+++ server.js
@@ -3,13 +3,25 @@ const express     = require('express');
 const logger      = require('morgan');
 
 const app    = express();
-const PORT   = process.argv[2] || process.env.port || 3000;
+
+function checkMinVersion(minVersion) {
+  const greaterThanMin = (x,i) => (minVersion[i] <= +x);
+  // get the node version in numbers
+  if (!process.version.split(/\D/).filter(x=>x).every(greaterThanMin)) {
+    throw `unsupported version! Update Node to v${minVersion.join('.')} to proceed. Current version: ${process.version}`
+
+  }
+}
+
 
 // set up some logging
 app.use(logger('dev'));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// MAKE SURE we're using 8081
+app.set('port', process.argv[2] || process.env.PORT || 8081);
 
 // generic error handler
 app.use((err, req, res, next) => {
@@ -22,5 +34,5 @@ app.get('/', (req,res) => res.render('pages/home'));
 
 
 // Let's go!
-app.listen(PORT);
+app.listen(app.settings.port, ()=>checkMinVersion([7,6,0]));
 // app.listen(PORT, () => console.log(Object.keys(process.env).sort(), isDev));
